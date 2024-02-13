7b70db48b117e39689d32af7060943f8be9f5b48
Pankaj Ajmera <coderajmera@gmail.com>
2024-02-13 11:22:14 +0530

    Day3 -Custom React and BTS of React

--- Day1/Day1-install/src/main.jsx
+++ Day1/Day1-install/src/main.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+
+const anotherElement =(
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const ReactElement =React.createElement(
+  'a',
+  {href:'https://google.com' ,target :'_blank'},
+  'Clime me to visit google'
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App/>
+  
 )
